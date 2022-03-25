40a0931c05a0d3556155a50c83ef9162fa2f5b82
Franklyn Urey Guardado <franklyn.urey@kiumas.net>
2022-03-25 17:09:00 -0600

    Storing Original Data

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ class App extends Component {
   constructor(){
     super();
 
-    this.state = {monsters:[],};
-    console.log('1 Constructor');
+    this.state = {
+      monsters:[],
+      searchField:''
+    };
   }
 
   componentDidMount(){
-    console.log('3 componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(() => {
@@ -25,15 +26,23 @@ class App extends Component {
   }
 
   render() {
-    console.log('2 Render');
+    const filteredMonsters = this.state.monsters.filter((item) => {
+      return item.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input className='search-box' 
           type='search' placeholder='search monsters'
           onChange={(event) => {
-            console.log(event.target.value);
-          }}/>
-        {this.state.monsters.map((monster)=>{
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return {searchField};
+            });            
+          }}
+        />
+
+        {filteredMonsters.map((monster)=>{
           return(
             <div key={monster.id}>
               <h1>{monster.name}</h1>
