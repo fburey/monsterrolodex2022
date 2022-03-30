5216e141967b83a3c4bd6247678019d3e1f290f0
Franklyn Urey Guardado <franklyn.urey@kiumas.net>
2022-03-30 00:12:46 -0600

    Hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState('a'); //[value, setValue]
-  const [monsters, setMonsters] = useState([]); //[value, setValue]
-  console.log('render');
+  const [monsters, setMonsters] = useState([]); 
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response) => response.json())
-  .then((users) => setMonsters(users));
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  },[]);
 
-
-  console.log(searchField);
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((item) => {
+      return item.name.toLowerCase().includes(searchField);
+    });
+    
+    setFilteredMonsters(newFilteredMonsters);
+    console.log('effect is firing');
+  },[monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
-
-  const filteredMonsters = monsters.filter((item) => {
-    return item.name.toLowerCase().includes(searchField);
-  });  
-
+  
   return(
     <div className="App">
     <h1 className="app-title">Monster Rolodex</h1>
@@ -32,6 +36,7 @@ const App = () => {
     className = 'monsters-search-box'
     placeholder = 'search monsters'
     onChangeHandler = {onSearchChange}/>        
+
     <CardList monsters = {filteredMonsters}/>
   </div>
   )
